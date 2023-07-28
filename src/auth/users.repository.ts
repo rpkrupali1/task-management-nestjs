d6687aba761f1bba558497784ddc95952c3d36e8
rpkrupali <rpkrupali@gmail.com>
2023-07-28 07:00:09 -0500

    handled for duplicate users

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,4 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
@@ -12,7 +16,14 @@ export class UsersRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { username, password } = authCredentialsDto;
     const user = this.create({ username, password });
-
-    await this.save(user);
+    try {
+      await this.save(user);
+    } catch (error) {
+      console.log(error.code);
+      if (error.code === '23505') {
+        // duplicate username
+        throw new ConflictException('Username already exist');
+      } else throw new InternalServerErrorException();
+    }
   }
 }
