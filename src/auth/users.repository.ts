e62cdbf5130724ed99e8d7ee2b02dae7f12155d6
rpkrupali <rpkrupali@gmail.com>
2023-07-28 06:37:01 -0500

    added signup service

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,10 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { DataSource, Repository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class UsersRepository extends Repository<User> {
   constructor(private datasource: DataSource) {
     super(User, datasource.createEntityManager());
+  }
+
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+
+    await this.save(user);
   }
 }
